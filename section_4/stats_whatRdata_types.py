ac91fbade70f1d268035544a89cac82d932878d3
KsaweryKapela <ksawerykapela97@gmail.com>
2022-11-01 16:01:07 +0100

    Did section 4 of course.

---
+++ section_4/stats_whatRdata_types.py
@@ -0,0 +1,20 @@
+# data numerical (here as a list)
+numdata = [1, 7, 17, 1717]
+
+# character / string
+chardata = 'xyz'
+
+# double-quotes also fine
+strdata = "x"
+
+# boolean (aka logical)
+logitdata = True  # notice capitalization!
+
+# a list can be used like a MATLAB cell
+listdata = [[3, 4, 34], 'hello', 4]
+
+# dict (kindof similar to MATLAB structure)
+dictdata = dict()
+dictdata['name'] = 'Mike'
+dictdata['age'] = 25
+dictdata['occupation'] = 'Nerdoscientist'
